69539cccd45c2b8e3711017c45065c682579ad2a
Prince-baba <princepatel.uk@gmail.com>
2024-08-21 10:38:25 +0530

    modified:   app/update-prompt/page.jsx

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Suspense } from 'react';
 import { useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 
@@ -70,6 +71,12 @@ const EditPrompt = () => {
         handleSubmit={updatePrompt}
     />
   )
-}
+};
 
-export default EditPrompt
+const UpdatePromptWithSuspense = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <UpdatePrompt />
+  </Suspense>
+);
+
+export default UpdatePromptWithSuspense;
